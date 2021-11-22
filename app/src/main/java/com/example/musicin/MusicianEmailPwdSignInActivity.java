8a7ab8c5342df0a0fc1d14814686511b8d411322
Frederico Ferreira <fma.ferreira@campus.fct.unl.pt>
2021-11-22 16:04:12 +0000

    nothing

--- app/src/main/java/com/example/musicin/MusicianEmailPwdSignInActivity.java
+++ app/src/main/java/com/example/musicin/MusicianEmailPwdSignInActivity.java
@@ -2,7 +2,12 @@ package com.example.musicin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MusicianEmailPwdSignInActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MusicianEmailPwdSignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musician_email_pwd_sign_in);
+
+        TextInputEditText email = findViewById(R.id.email_ed_txt);
+        TextInputEditText password = findViewById(R.id.password_ed_txt);
+        TextInputEditText passwordConfirmed = findViewById(R.id.password_ed_txt3);
+
+        MaterialButton login = findViewById(R.id.login_bttn);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent());
+            }
+        });
     }
 }
