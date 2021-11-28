802d9e82b06bdb9dfe9a5d41e9e0d6f5412df687
PC <a.bia@campus.fct.unl.pt>
2021-11-28 23:10:52 +0000

    Started Add Request To Form Band Page

---
+++ app/src/main/java/com/example/musicin/AddFormBandRequestActivity.java
@@ -0,0 +1,14 @@
+package com.example.musicin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFormBandRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_form_band_request);
+    }
+}
