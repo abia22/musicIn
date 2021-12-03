7cf75d64856a60821dc43ff5b57f70ae5fe8025d
PC <a.bia@campus.fct.unl.pt>
2021-12-03 10:42:02 +0000

    On Add Request To Form Band Page users can now add needed instruments in a list one by one

--- app/src/main/java/com/example/musicin/AddFormBandRequestActivity.java
+++ app/src/main/java/com/example/musicin/AddFormBandRequestActivity.java
@@ -3,6 +3,14 @@ package com.example.musicin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddFormBandRequestActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class AddFormBandRequestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_form_band_request);
+
+        EditText instruments_edt = findViewById(R.id.needed_instruments_ed_txt);
+        ListView instruments_lv = findViewById(R.id.instruments_lv);
+        ImageView addItem_bttn = findViewById(R.id.add_item);
+        ArrayList<String> instrumentsList = new ArrayList<>();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, instrumentsList);
+        instruments_lv.setAdapter(arrayAdapter);
+
+        addItem_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = instruments_edt.getText().toString();
+                if(text == null || text.length() == 0){
+                    Toast.makeText(getApplicationContext(), "Please enter an instrument", Toast.LENGTH_SHORT).show();
+                } else if(instrumentsList.contains(text)) {
+                    Toast.makeText(getApplicationContext(), "This instrument was already added", Toast.LENGTH_SHORT).show();
+                } else {
+                    instruments_edt.setText("");
+                    instrumentsList.add(text);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/musicin/fragments/SearchMembersFragment.java
+++ app/src/main/java/com/example/musicin/fragments/SearchMembersFragment.java
@@ -1,5 +1,6 @@
 package com.example.musicin.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +10,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.musicin.AddFormBandRequestActivity;
 import com.example.musicin.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SearchMembersFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search_members, container, false);
+        View view = inflater.inflate(R.layout.fragment_search_members, container, false);
+
+        FloatingActionButton add_request_bttn = view.findViewById(R.id.add_request_fab);
+        add_request_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), AddFormBandRequestActivity.class));
+            }
+        });
+
+        return view;
     }
 }
